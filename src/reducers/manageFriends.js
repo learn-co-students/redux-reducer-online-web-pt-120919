30dc5645cbf468b691ac52829279578559da0a33
seung_kim <seunglives@hotmail.com>
2020-09-16 08:24:53 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: []}, action){
+    if (action.type === "ADD_FRIEND") {
+        return {...state, 
+                friends: [
+                    ...state.friends, 
+                    action.friend
+                ]
+        }
+    }
+    else if (action.type === "REMOVE_FRIEND") {
+        const removedFriend = state.friends.findIndex(friend => friend.id === action.id)
+        return {...state, 
+                friends: [
+                    ...state.friends.slice(0, removedFriend),
+                    ...state.friends.slice(removedFriend + 1)
+                ]    
+        }
+    }
+    else {
+        return state;
+    }
+
 }
