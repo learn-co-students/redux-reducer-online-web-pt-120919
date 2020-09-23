d5447e85fbafb1b8d9c844ea492abc3540004fa9
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-09-23 21:50:31 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -12,34 +12,13 @@ export function manageFriends(state = {friends: []}, action){
       )
       
     case "REMOVE_FRIEND":
-      console.log("======================================")
-      console.log("======ORIGINAL=======")
-      console.log(state)
-      console.log("======FRIEND=======")
-      console.log(action.id)
+      let arr = {...state}.friends
+      var filtered = arr.filter(function(value, index, arr){ return value.id != action.id;});
       
-      
-      var arr = state.friends;
-      for( var i = 0; i < arr.length; i++){
-        if ( arr[i].id === action.id) { 
-          arr.splice(i, 1); 
-          i--; 
-        }
-      }
-      console.log("======MODIFIED=======")
-      console.log(
-        { 
-          ...state,
-          friends: arr
-        }
-            
-      )
-      console.log("======ORIGINAL=======")
-      console.log(state)
       return (
          { 
           ...state,
-          friends: arr
+          friends: filtered
         }
       )
       
